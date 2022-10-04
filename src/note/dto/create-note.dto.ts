c572dd14bc403705fe05f5b4dfc971732a8e26a9
AlinaSyniavska <alina22syniavska@gmail.com>
2022-10-04 11:20:16 +0300

    add validation dto

--- src/note/dto/create-note.dto.ts
+++ src/note/dto/create-note.dto.ts
@@ -1,8 +1,39 @@
+import {
+    ArrayMaxSize,
+    ArrayMinSize,
+    IsArray,
+    IsDateString,
+    IsEnum,
+    IsOptional,
+    IsString,
+    Length
+} from "class-validator";
+import {noteCategoryEnum} from "../../constants/noteCategoryEnum";
+import {noteStatusEnum} from "../../constants/noteStatusEnum";
+
 export class CreateNoteDto {
+    @IsString()
+    @Length(2, 100)
     public name: string;
+
+    @IsDateString()
     public created: Date;
+
+    @IsString()
+    @IsEnum(noteCategoryEnum)
     public category: string;
+
+    @IsString()
+    @Length(2, 300)
     public content: string;
+
+    @IsArray()
+    @IsOptional()
+    @ArrayMinSize(0)
+    @ArrayMaxSize(0)
     public dates: Date[];
-    public noteStatus: string;//not required
+
+    @IsString()
+    @IsEnum(noteStatusEnum)
+    public noteStatus: string;
 }
--- src/note/dto/update-note.dto.ts
+++ src/note/dto/update-note.dto.ts
@@ -1,8 +1,44 @@
+import {
+    ArrayMaxSize, ArrayMinSize,
+    IsArray,
+    IsDateString,
+    IsEnum,
+    IsOptional,
+    IsString,
+    Length
+} from "class-validator";
+import {noteCategoryEnum} from "../../constants/noteCategoryEnum";
+import {noteStatusEnum} from "../../constants/noteStatusEnum";
+
 export class UpdateNoteDto {
+    @IsOptional()
+    @IsString()
+    @Length(2, 100)
     public name: string;
-    public created: string;
+
+    @IsOptional()
+    @IsDateString()
+    public created: Date;
+
+    @IsOptional()
+    @IsString()
+    @IsEnum(noteCategoryEnum)
     public category: string;
+
+    @IsOptional()
+    @IsString()
+    @Length(2, 300)
     public content: string;
-    public dates: string[];
-    public noteStatus: string;//not required
+
+    @IsOptional()
+    @IsArray()
+    @IsString({ each: true })
+    @ArrayMinSize(2)
+    @ArrayMaxSize(2)
+    public dates: Date[];
+
+    @IsOptional()
+    @IsString()
+    @IsEnum(noteStatusEnum)
+    public noteStatus: string;
 }
